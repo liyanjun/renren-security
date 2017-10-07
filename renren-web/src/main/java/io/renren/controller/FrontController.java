1b9e1ab8f94701022b8874a521cce1b0db155297
liyanjun <280378807@qq.com>
2017-10-07 11:18:08 +0800

    整合支付宝支付功能

--- renren-web/src/main/java/io/renren/controller/FrontController.java
+++ renren-web/src/main/java/io/renren/controller/FrontController.java
@@ -1,9 +1,17 @@
 package io.renren.controller;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.domain.AlipayTradeWapPayModel;
+import com.alipay.api.request.AlipayTradeWapPayRequest;
+import io.renren.config.AlipayConfig;
 import io.renren.entity.*;
 import io.renren.service.*;
 import io.renren.utils.*;
 import org.apache.shiro.SecurityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +36,8 @@ import java.util.Map;
 @Controller
 @RequestMapping("/front")
 public class FrontController extends AbstractController {
+
+    Logger logger = LoggerFactory.getLogger(FrontController.class);
 
     @Autowired
     private GamePriceService gamePriceService;
@@ -84,11 +97,13 @@ public class FrontController extends AbstractController {
      * @return
      */
     @RequestMapping("/submit")
-    public String submit(GameOrderEntity gameOrderEntity, HttpServletRequest request, Model model) {
+    public void submit(GameOrderEntity gameOrderEntity, HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
         List<GamePropertyValueEntity> valueList = new ArrayList<>();
         List<GamePropertyEntity> gamePropertyList = gamePropertyService.queryAll();
+        String orderName = "";
         for (GamePropertyEntity gamePropertyEntity : gamePropertyList) {
             String value = request.getParameter(gamePropertyEntity.getName());
+            orderName += value;
             GamePropertyValueEntity gamePropertyValueEntity = new GamePropertyValueEntity();
             gamePropertyValueEntity.setGamePropertyId(gamePropertyEntity.getId());
             gamePropertyValueEntity.setValue(value);
@@ -98,20 +113,67 @@ public class FrontController extends AbstractController {
         gameOrderEntity.setTotalAmount(gamePriceEntity.getPrice().multiply(BigDecimal.valueOf(gameOrderEntity.getGamePriceNumber())));
         gameOrderEntity.setCreatorId(-1L);
         gameOrderService.save(gameOrderEntity, valueList);
-        int type = 0;
-        String userAgent = request.getHeader("user-agent").toLowerCase();
-        if (userAgent.indexOf("micromessenger") > -1) {//微信客户端
-            type = 1;
-        } else {
-            type = 2;
-        }
-        List<GamePayImgEntity> list = gamePayImgService.queryByMatchPrice(gameOrderEntity.getTotalAmount(), type);
-        if (list.size() < 0) {
-            throw new RRException("没有配置付款图片。");
-        }
+        //起调支付宝支付
+        // 商户订单号，商户网站订单系统中唯一订单号，必填
+        String out_trade_no = new String(gameOrderEntity.getId().toString().getBytes("ISO-8859-1"),"UTF-8");
+        // 订单名称，必填
+        String subject = new String((orderName+gameOrderEntity.getId()).getBytes("ISO-8859-1"),"UTF-8");
+        System.out.println(subject);
+        // 付款金额，必填
+        String total_amount=new String(gameOrderEntity.getTotalAmount().toString().getBytes("ISO-8859-1"),"UTF-8");
+//        // 商品描述，可空
+//        String body = new String(request.getParameter("WIDbody").getBytes("ISO-8859-1"),"UTF-8");
+//        // 超时时间 可空
+//        String timeout_express="2m";
+        // 销售产品码 必填
+        String product_code="QUICK_WAP_PAY";
+        /**********************/
+        // SDK 公共请求类，包含公共请求参数，以及封装了签名与验签，开发者无需关注签名与验签
+        //调用RSA签名方式
+        AlipayClient client = new DefaultAlipayClient(AlipayConfig.URL, AlipayConfig.APPID, AlipayConfig.RSA_PRIVATE_KEY, AlipayConfig.FORMAT, AlipayConfig.CHARSET, AlipayConfig.ALIPAY_PUBLIC_KEY,AlipayConfig.SIGNTYPE);
+        AlipayTradeWapPayRequest alipay_request=new AlipayTradeWapPayRequest();
 
-        model.addAttribute("img", list.get(0));
-        return "pay.html";
+        // 封装请求支付信息
+        AlipayTradeWapPayModel model=new AlipayTradeWapPayModel();
+        model.setTotalAmount(total_amount);
+//        model.setBody(body);
+//        model.setTimeoutExpress(timeout_express);
+        model.setProductCode(product_code);
+        alipay_request.setBizModel(model);
+        // 设置异步通知地址
+        alipay_request.setNotifyUrl(AlipayConfig.notify_url);
+        // 设置同步地址
+        alipay_request.setReturnUrl(AlipayConfig.return_url);
+
+        // form表单生产
+        String form = "";
+        try {
+            // 调用SDK生成表单
+            form = client.pageExecute(alipay_request).getBody();
+            response.setContentType("text/html;charset=" + AlipayConfig.CHARSET);
+            response.getWriter().write(form);//直接将完整的表单html输出到页面
+            response.getWriter().flush();
+            response.getWriter().close();
+        } catch (AlipayApiException e) {
+            logger.error("订单提交支付错误",e);
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            logger.error("订单提交支付错误",e);
+        }
+//        int type = 0;
+//        String userAgent = request.getHeader("user-agent").toLowerCase();
+//        if (userAgent.indexOf("micromessenger") > -1) {//微信客户端
+//            type = 1;
+//        } else {
+//            type = 2;
+//        }
+//        List<GamePayImgEntity> list = gamePayImgService.queryByMatchPrice(gameOrderEntity.getTotalAmount(), type);
+//        if (list.size() < 0) {
+//            throw new RRException("没有配置付款图片。");
+//        }
+//
+//        model.addAttribute("img", list.get(0));
+//        return "pay.html";
     }
 
 }
